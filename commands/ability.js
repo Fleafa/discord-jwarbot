0bb069884300708249f8206573b73401a7b0d850
Fleafa <Fleafa@users.noreply.github.com>
2022-12-22 20:38:16 +0000

    added /ability command

---
+++ commands/ability.js
@@ -0,0 +1,101 @@
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, userMention } = require('discord.js');
+// const wait = require('node:timers/promises').setTimeout;
+// eslint-disable-next-line no-unused-vars
+const { bold, italic, underscore, blockQuote, codeBlock } = require('discord.js');
+const libraryFile = require('../rules/abilities.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ability')
+		.setDescription('Replies with ability definition.')
+		.addStringOption(option =>
+			option.setName('ability_name')
+				.setDescription('The ability to search for.')
+				.setAutocomplete(true)
+				.setRequired(true)),
+
+	async autocomplete(interaction) {
+		const focusedValue = interaction.options.getFocused().toLowerCase();
+		const abilityList = [];
+
+		for (const ability in libraryFile) {
+			abilityList.push(libraryFile[ability]['name'].toLowerCase());
+		}
+
+		const filtered = abilityList.filter(choice => choice.startsWith(focusedValue));
+		if (focusedValue) {
+			await interaction.respond(
+				filtered.map(choice => ({ name: choice, value: choice })),
+			);
+		}
+	},
+
+	async execute(interaction) {
+
+		let getInput = interaction.options.getString('ability_name');
+
+		// prompt user to use the command correctly; later I may add a pop-up modal when the input is empty
+		if (!getInput) {
+			await interaction.reply({ content: 'Error: no ability provided.\nPlease type "/ability *ability name*" to request the ability definition.', ephemeral: true });
+			return;
+		}
+		else {
+			getInput = getInput.toLowerCase();
+			console.log(getInput);
+		}
+
+		// prepend "abilitys-", and replace spaces with underscores
+		const abilityId = 'ability-' + getInput.replace(/ /g, '_');
+		let abilityName;
+
+		try {
+			abilityName = libraryFile[abilityId]['name'];
+		}
+		catch (err) {
+			console.log('err: no match found for "' + abilityId + '"');
+			await interaction.reply({ content: 'Error: no ability matching "' + getInput + '" found.\nPlease the autoprediction to find a valid ability.', ephemeral: true });
+			return;
+		}
+
+		const abilityDesc = libraryFile[abilityId]['description'];
+		// const abilityBSID = libraryFile[abilityId]['bsid'];
+		// const abilityRefs = [];
+		const abilityRB = libraryFile[abilityId]['rulebook'];
+		const abilityRBr1 = libraryFile[abilityId]['rulebookr1'];
+		const abilityRev = libraryFile[abilityId]['revision'];
+		const abilityStatus = libraryFile[abilityId]['status'];
+
+		let abilityDefinition = '';
+
+		abilityDefinition = bold(abilityName) + codeBlock(abilityDesc) + italic('**Revision:** ' + abilityRev + ' (' + abilityStatus + ')\t**Risen Sun:** p.' + abilityRB + '\t**2022 reprint:** p.' + abilityRBr1);
+
+		const row = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('ability_share')
+					.setLabel('share in this channel')
+					.setStyle(ButtonStyle.Primary),
+			);
+
+		await interaction.reply({ content: abilityDefinition, components: [row], ephemeral: true, fetchReply: true });
+
+		const filter = i => i.customId === 'ability_share';
+		const componentCollector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
+
+		componentCollector.on('collect', async i => {
+			await interaction.channel.send(userMention(interaction.user.id) + ' used /ability:\n' + abilityDefinition);
+			await i.update({ content: 'Shared definition of ' + abilityName + ' to channel.', components: [] });
+			console.log(abilityName + ' definition shared to ' + interaction.channel);
+			componentCollector.stop();
+		});
+
+		componentCollector.on('end', async collected => {
+			console.log(`Collected ${collected.size} interactions.`);
+			if (!collected.size) {
+				await row.components[0].setDisabled(true).setLabel('60s timeout');
+				await interaction.editReply({ content: abilityDefinition, components: [row] });
+			}
+		});
+	},
+
+};
