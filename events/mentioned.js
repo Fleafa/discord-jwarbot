f4514e80ce5665b477ae16f759d2f172a8cc7b53
Fleafa <Fleafa@users.noreply.github.com>
2022-12-22 20:38:26 +0000

    updated help text

--- events/mentioned.js
+++ events/mentioned.js
@@ -6,7 +6,7 @@ module.exports = {
 	async execute(message) {
 		const fbotID = new RegExp('(<@!?' + clientId + '>)(?: |)');
 		const botIDmatch = message.content.match(fbotID);
-		const jwarbotTip = ':ninja: Hi there! I\'m JwarBot. I currently support the following commands :ninja:\n**/warband** will prompt you to paste a Battlescribe Warband you\'ve copied using Share > Chat. It will then send it back to you in a prettier format.\n**/trait *trait_name*** & **/state *state_name*** will try to autocomplete the corresponding rule name. When you send the command, I\'ll reply with that rule\'s current definition.\n*Coming soon(ish): /ability*';
+		const jwarbotTip = ':ninja: Hi there! I\'m JwarBot. I currently support the following commands :ninja:\n**/warband** will prompt you to paste a Battlescribe Warband you\'ve copied using Share > Chat. It will then send it back to you in a prettier format.\n**/trait *trait_name***, **/state *state_name*** **/ability *ability_name***, & **/feat *feat_name*** will try to autocomplete the corresponding rule name. When you send the command, I\'ll reply with that rule\'s current definition.\n*Coming soon(ish): /concept*';
 		if (botIDmatch) {
 			return message.channel.send(jwarbotTip);
 		}
