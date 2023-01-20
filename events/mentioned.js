799b6b14d87a9c81ff513954594374578dae3b67
Fleafa <Fleafa@users.noreply.github.com>
2023-01-20 08:50:49 +0000

    cleaner formatting

--- events/mentioned.js
+++ events/mentioned.js
@@ -6,7 +6,7 @@ module.exports = {
 	async execute(message) {
 		const fbotID = new RegExp('(<@!?' + clientId + '>)(?: |)');
 		const botIDmatch = message.content.match(fbotID);
-		const jwarbotTip = ':ninja: Hi there! I\'m JwarBot v0.5 :ninja:\nI currently support the following commands:\n**/trait** *trait_name*\n**/state** *state_name*\n**/ability** *ability_name*\n**/feat** *feat_name*\nTry to autocomplete the corresponding rule name. When you send the command, I\'ll reply with that rule\'s current definition.\n**/warband**\nPrompts you to paste a Battlescribe Warband copied using Share > Chat. It will then send it back to you in a prettier format.\n*Coming soon(ish): /concept*';
+		const jwarbotTip = ':ninja: Hi there! I\'m JwarBot v0.5 :ninja:\nI currently support the following commands:\n**/trait** *trait_name*\n**/state** *state_name*\n**/ability** *ability_name*\n**/feat** *feat_name*\n\t I\'ll try to autocomplete the corresponding rule name and reply with that rule\'s current definition.\n\n**/warband**\n\tI\'ll prompt you to paste your Battlescribe Warband (copied using its Share > Chat function) and send it back to you in a prettier, more legible format.';
 		if (botIDmatch) {
 			return message.channel.send(jwarbotTip);
 		}
